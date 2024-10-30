13ace28375713bbb0d7b8ecc8d03780b537dbfd7
Natalia Keller <natalia.kellermacielfarah@stud.itsaltoadriatico.it>
2024-10-30 16:55:55 +0100

    ciaooo2

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, world! tumammaThe server is running on port 3000.');
+    res.end('Hello, world! aijsdoajfoajf server is running on port 3000.');
 });
 
 server.listen(port, () => {
